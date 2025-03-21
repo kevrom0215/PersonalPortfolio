399f5d66bc0dff1681719171b3ecbb89a705018c
Kenneth Araga <kennetharaga@gmail.com>
2025-03-21 15:52:17 +0800

    added route

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: "/PersonalPortfolio/",
+  base: "/PersonalPortfolio/", // Corrected base path
   plugins: [
     react(),
     runtimeErrorOverlay(),
